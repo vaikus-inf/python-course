82d73585e1eab9c5a572852ac746873f3c33076a
Ilya Poklitar <vaikus.gm@gmail.com>
2022-01-17 18:44:03 +1100

    change section_6_tasks.py

--- section_6_tasks.py
+++ section_6_tasks.py
@@ -95,7 +95,7 @@ import math
 
 class Circle:
     
-    def __init__(self, radius):
+    def __init__(self, radius = 0):
         self.radius = radius    
     
     def get_area(self):
@@ -132,6 +132,33 @@ class Beverage:
         sorted_ingredients.append('Fusion') if len(sorted_ingredients) > 1 else sorted_ingredients.append('Smoothie')
         return ' '.join(sorted_ingredients)
 
+s1 = Beverage(['Mango', 'Banana', 'Strawberries'])
+print(s1.get_cost())
+print(s1.get_price())
+print(s1.get_name())
+
+# Задача № 6 (решение из курса)
+prices = {'Strawberries' : 1.5, 'Banana' : 0.5, 'Mango' : 2.5,
+		'Blueberries' : 1, 'Raspberries' : 1, 'Apple' : 1.75,
+		'Pineapple' : 3.5}
+
+class Beverage:
+
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+        self.cost = sum([prices[fruit] for fruit in self.ingredients])
+        self.price = self.cost * 2.5
+
+    def get_cost(self):
+        return f'${self.cost:.2f}'
+    
+    def get_price(self):
+        return f'${self.price:.2f}'
+    
+    def get_name(self):
+        lst = sorted([i.replace('ies', 'y') for i in self.ingredients])
+        return f'{" ".join(lst)} {"Fusion" if len(lst) > 1 else "Smoothie"}'
+	
 s1 = Beverage(['Mango', 'Banana', 'Strawberries'])
 print(s1.get_cost())
 print(s1.get_price())
