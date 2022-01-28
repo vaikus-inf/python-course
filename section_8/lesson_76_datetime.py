2fd6b4abf1aac6f3bfee6ec20977c40b41861a9d
Ilya Poklitar <vaikus.gm@gmail.com>
2022-01-28 17:43:06 +1100

    delete file date_time.py and add new file lesson_76_datetime.py

--- section_8/lesson_76_datetime.py
+++ section_8/lesson_76_datetime.py
@@ -1,7 +1,43 @@
+# Работа с модулем datetime
 from datetime import datetime
 from datetime import date
 from datetime import time
 from datetime import timedelta
+
+d1 = date(2019, 3, 12)
+print(d1)
+print(d1.year)
+print(d1.month)
+print(d1.day)
+
+t1 = time(23, 10, 59)
+print(t1)
+print(t1.hour)
+print(t1.minute)
+print(t1.second)
+
+print(date.today())
+
+print(date.max)
+print(date.min)
+print(time.max)
+print(time.min)
+
+dt = datetime(2019, 3, 12, 15, 17)
+print(dt)
+print(dt.date().year)
+print(dt.time().hour)
+
+print(datetime.max)
+print(datetime.min)
+
+now = datetime.now()
+print(now)
+print(now.year)
+print(now.month)
+
+new_date = now.replace(year = 2021)
+print(new_date)
 
 # Парсинг даты из строки
 dt = datetime.strptime('30/08/2018', '%d/%m/%Y')
@@ -25,7 +61,7 @@ now = datetime.now()
 print(now.strftime('%Y-%m-%d (%a)'))
 print(now.strftime('%Y %B %d число (%A)'))
 
-# Работа с timedelta
+# Работа с функцией timedelta
 delta1 = timedelta(days = 3, hours = 2, minutes = 10)
 print(delta1)
 
